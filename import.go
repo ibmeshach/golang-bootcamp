57f9854a6d343d6f1bccf9f2404989a4602be5a3
ibadin <ibmeshach@gmail.com>
2025-04-27 02:27:02 +0100

    go basics

---
+++ import.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	https "net/http"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	resp, err := https.Get("https://jsonplaceholder.typicode.com/posts/1")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("HTTP Response Status: ", resp.Status)
+}
