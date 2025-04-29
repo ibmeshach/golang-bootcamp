4cf3c5a91b072debb677cfd0944ca0ba49d930a7
ibadin <ibmeshach@gmail.com>
2025-04-29 18:48:31 +0100

    constants

---
+++ basics/constants.go
@@ -0,0 +1,16 @@
+package basics
+
+const pi = 3.14
+const GRAVITY = 9.81
+
+func ConstantsMain() {
+	const dats int = 7
+
+	const (
+		monday = 1
+		tuesday = 2
+		wednesday = 3
+		thursday int = 4
+	)
+
+}
